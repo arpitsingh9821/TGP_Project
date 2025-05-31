3ac421cb44ac3725e7ae59fbbb8419a1355e1c0e
arpitsingh9821 <158504080+arpitsingh9821@users.noreply.github.com>
2025-05-31 15:21:25 +0530

    new

--- Backend/index.js
+++ Backend/index.js
@@ -14,10 +14,6 @@ app.get('/', (req, res) => {
   res.send('TGP Backend API Running');
 });
 
-db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)");
-});
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
